ccd9811b27d2b7148b51c16c8c8e737a2ba38441
bBobxx <30625266+bBobxx@users.noreply.github.com>
2018-09-07 12:43:28 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,85 @@
+import os
+import os.path as osp
+import sys
+import numpy as np
+
+class Config:
+    username = 'default'
+
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    this_dir_name = cur_dir.split('/')[-1]
+    root_dir = os.path.join(cur_dir, '..')
+
+    proj_name = this_dir_name
+
+    # output path
+    output_dir = os.path.join(root_dir, 'logs', username + '.' + this_dir_name)
+    model_dump_dir = osp.join(output_dir, 'model_dump')
+
+    display = 1
+
+    lr = 5e-4
+    lr_gamma = 0.5
+    lr_dec_epoch = 60
+
+    epoch_size = 60000 
+    optimizer = 'adam'
+
+    batch_size = 2 
+    weight_decay = 1e-5
+
+    step_size = epoch_size * lr_dec_epoch
+    max_itr = epoch_size * 400
+    double_bias = False
+
+    dpflow_enable = True
+    nr_dpflows = 10
+
+    gpu_ids = '0'
+    nr_gpus = 1
+    continue_train = False
+
+    def get_lr(self, itr):
+        lr = self.lr * self.lr_gamma ** (itr // self.step_size)
+        return lr
+
+    def set_args(self, gpu_ids, continue_train=False):
+        self.gpu_ids = gpu_ids
+        self.nr_gpus = len(self.gpu_ids.split(','))
+        self.continue_train = continue_train
+        os.environ["CUDA_VISIBLE_DEVICES"] = self.gpu_ids
+        print('>>> Using /gpu:{}'.format(self.gpu_ids))
+
+    bn_train = True
+    init_model = osp.join(root_dir, 'inpu', 'imagenet_weights', 'res50.ckpt')
+
+    nr_skeleton = 17
+    img_path = os.path.join(root_dir, 'input', 'coco_train', 'MSCOCO', 'images')
+    symmetry = [(1, 2), (3, 4), (5, 6), (7, 8), (9, 10), (11, 12), (13, 14), (15, 16)]
+
+    imgExtXBorder = 0.1
+    imgExtYBorder = 0.15
+    min_kps = 1
+
+    use_seg = False
+
+    data_aug = True # has to be true
+    nr_aug = 4
+
+    pixel_means = np.array([[[102.9801, 115.9465, 122.7717]]]) # BGR
+    pixel_norm = True
+    data_shape = (256, 256) #height, width
+    output_shape = (64, 64) #height, width
+    gaussain_kernel = (7, 7)
+    #
+    gk15 = (15, 15)
+    gk11 = (11, 11)
+    gk9 = (9, 9)
+    gk7 = (7, 7)
+
+    gt_path = osp.join(root_dir, 'input', 'coco-train-val2017','person_keypoints_val2017.json')
+    det_path = osp.join(root_dir, 'data', 'COCO', 'dets', 'person_detection_minival411_human553.json')
+
+cfg = Config()
+
+
---
+++ train.py
@@ -0,0 +1,147 @@
+import tensorflow as tf
+import numpy as np
+from config import cfg
+import math
+share_count = 0
+
+
+def conv_bn_layer(input_fm, filters, kernel_size, stride, training, sdd = None, reuse=False, name=None):
+    conv = tf.layers.conv2d(
+        inputs=input_fm, filters=filters, kernel_size=kernel_size, strides=stride,
+        padding=('SAME' if stride == 1 else 'VALID'), use_bias=False,
+        kernel_initializer=tf.random_normal_initializer(stddev=sdd) if sdd is not None else
+        tf.variance_scaling_initializer(scale=4.0, mode='fan_avg'), reuse=reuse)
+    bn = tf.layers.batch_normalization(inputs=conv, training=training, fused=True, reuse=reuse)
+    bn_relu = tf.nn.relu(bn)
+    return bn_relu
+
+
+def compute_standard_deviation(c_i, c_o, n_i, n_o):
+    # initialize  standard deviation according equation 15
+    return 2.0/math.sqrt(c_i*n_i + c_o*n_o)
+
+
+def prm(input_fm, is_train, f_i=3, f_o=3):
+    share_input = conv_bn_layer(input_fm, 28, 1, 1, is_train, sdd=compute_standard_deviation(f_i, 4, 256, 3*28))
+    sz = tf.shape(share_input)
+    out_sz = (sz[1], sz[2])
+    ratio0 = conv_bn_layer(input_fm, 28, 1, 1, is_train, compute_standard_deviation(f_i, 4, 256, 3*28))
+    ratio1, _, _ = tf.nn.fractional_max_pool(share_input, [1.0, 1.189, 1.189, 1.0])
+    ratio2, _, _ = tf.nn.fractional_max_pool(share_input, [1.0, 1.414, 1.414, 1.0])
+    ratio3, _, _ = tf.nn.fractional_max_pool(share_input, [1.0, 1.681, 1.681, 1.0])
+    ratio4, _, _ = tf.nn.fractional_max_pool(share_input, [1.0, 2.000, 2.000, 1.0])
+    #todo add share
+    global share_count
+    conv_share0 = conv_bn_layer(ratio0, 28, 3, 1, is_train, name='conv_share_'+str(share_count))
+    conv_share1 = conv_bn_layer(ratio1, 28, 3, 1, is_train, reuse=True, name='conv_share_'+str(share_count))
+    conv_share2 = conv_bn_layer(ratio2, 28, 3, 1, is_train, reuse=True, name='conv_share_'+str(share_count))
+    conv_share3 = conv_bn_layer(ratio3, 28, 3, 1, is_train, reuse=True, name='conv_share_'+str(share_count))
+    conv_share4 = conv_bn_layer(ratio4, 28, 3, 1, is_train, reuse=True, name='conv_share_'+str(share_count))
+    share_count += 1
+    upsample1 = tf.image.resize_bilinear(conv_share1, out_sz)
+    upsample2 = tf.image.resize_bilinear(conv_share2, out_sz)
+    upsample3 = tf.image.resize_bilinear(conv_share3, out_sz)
+    upsample4 = tf.image.resize_bilinear(conv_share4, out_sz)
+    additon = upsample1+upsample2+upsample3+upsample4
+    conv_out0 = conv_bn_layer(conv_share0, 256, 1, 1, is_train, compute_standard_deviation(1, f_o, 3*28, 256))
+    conv_out1 = conv_bn_layer(additon, 256, 1, 1, is_train, compute_standard_deviation(4, f_o, 3*28, 256))
+    identity_conv_map = conv_bn_layer(input_fm, 256, 1, 1, is_train, compute_standard_deviation(f_i, f_o, 256, 256))
+    return conv_out0+conv_out1+identity_conv_map
+
+
+def hourglass(input_fm, is_train):
+    fm_down = []
+    fm = input_fm
+    fm_down.append(fm)
+    for down_n in range(3):
+        fm = tf.layers.max_pooling2d(fm, pool_size=2, strides=2)
+        fm = prm(fm, is_train, 3, 6)
+        fm_down.append(fm)
+    # below is up block
+    last_fm = None
+    for block in reversed(fm_down):
+        bridge = prm(block, is_train)
+        if last_fm is not None:
+            sz = bridge.shape
+            upsample = prm(last_fm, is_train, 6, 3)
+            upsample = tf.image.resize_bilinear(upsample, (sz[-2], sz[-1]))
+            last_fm = bridge+upsample
+        else:
+            last_fm = bridge
+    last_fm = conv_bn_layer(last_fm, cfg.nr_skeleton, 1, 1, is_train)
+    score_maps = conv_bn_layer(last_fm, cfg.nr_skeleton, 1, 1, is_train)
+    hourglass_out0 = conv_bn_layer(score_maps, 256, 1, 1, is_train)
+    hourglass_out1 = conv_bn_layer(last_fm, 256, 1, 1, is_train)
+    hourglass_out = hourglass_out0+hourglass_out1+input_fm
+    return hourglass_out, score_maps
+
+
+def stack_hourglass(input_im, is_train):
+    conv0 = conv_bn_layer(input_im, 64, 7, 2, is_train)
+    prm0 = prm(conv0, is_train, 1, 1)
+    max_pool = tf.layers.max_pooling2d(prm0, 2, 2)
+    prm1 = prm(max_pool, is_train, 1, 3)
+    score_maps = []
+    hourglass_out = prm1
+    for stage in range(8):
+        hourglass_out, score_mapout = hourglass(hourglass_out, is_train)
+        score_maps.append(score_mapout)
+    return score_maps
+
+
+def make_data():
+    from COCOAllJoints import COCOJoints
+    from dataset import Preprocessing
+    def make_generator():
+        d = COCOJoints()
+        train_data, _ = d.load_data(1)
+        for data in train_data:
+            data_train = Preprocessing(data)
+            for i in range(4):
+                yield [data_train[j][i] for j in range(3)]
+    a = make_generator()
+    while True:
+        image = []
+        heatmap = []
+        valid = []
+        for i in range(cfg.batch_size):
+            try:
+                 b=next(a)
+            except:
+                yield [image, heatmap, valid]
+                break
+            else:
+                image.append(b[0])
+                heatmap.append(b[1])
+                valid.append(b[2])
+        yield [image, heatmap, valid]
+
+
+def train():
+    image = tf.placeholder(tf.float32, shape=[None, *cfg.data_shape, 3])
+    labels = tf.placeholder(tf.float32, shape=[None, *cfg.output_shape, cfg.nr_skeleton])
+    valids = tf.placeholder(tf.float32, shape=[None, cfg.nr_skeleton])
+    is_train = tf.placeholder(tf.bool)
+    stack_out = stack_hourglass(image, is_train)
+    global_loss = 0.
+    for i, (global_out, label) in enumerate(zip(stack_out, labels)):
+        global_label = label * tf.to_float(tf.greater(tf.reshape(valids, (-1, 1, 1, cfg.nr_skeleton)), 1.1))
+        global_loss += tf.reduce_mean(tf.square(global_out - global_label))
+    global_loss /= 2.
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    batch = tf.Variable(0)
+    learning_rate = tf.train.exponential_decay(
+        0.0007, batch * 3, 10, 0.95, staircase=True)
+    with tf.control_dependencies(update_ops):
+        train_step = tf.train.AdamOptimizer(learning_rate, 0.9).minimize(global_loss, global_step=batch)
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    with tf.Session(config=config) as sess:
+        init = tf.initialize_all_variables()
+        sess.run(init)
+        for data in make_data():
+            loss,_ = sess.run([global_loss, train_step], feed_dict={image:data[0], labels:data[1], valids:data[2], is_train:True})
+            print('loss is {}'.format(loss))
+
+if __name__ == '__main__':
+    train()
